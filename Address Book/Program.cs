10589ea83f962d17df20b22a8a00b2362f314b0a
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-09 09:02:44 +0530

    [Akshay]-Add Contacts

---
+++ Address Book/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Address_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addresbook addresbook= new Addresbook();
+            addresbook.CreateContact();
+        }
+    }
+}
